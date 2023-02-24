de5653bdca29a408e0ee645a7f091fc7b9431ca1
divyagarg123 <109232157+divyagarg123@users.noreply.github.com>
2023-02-25 01:34:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -159,7 +159,7 @@ def max_lr_finder(batch_size, device, trainset, model, optimizer, criterion, ass
         lr_finder.range_test(train_loader_noaug, start_lr=0.0001, end_lr=1, num_iter=200)
     else: 
         if assgn == 8:
-           lr_finder.range_test(train_loader_noaug, start_lr=0.001, end_lr=3, num_iter=200, weight_decay = 0.0001) 
+           lr_finder.range_test(train_loader_noaug, start_lr=0.001, end_lr=3, num_iter=200) 
     lr_finder.plot()
     min_loss = min(lr_finder.history['loss'])
     ler_rate = lr_finder.history['lr'][np.argmin(lr_finder.history['loss'], axis=0)]
@@ -188,7 +188,7 @@ def main():
         optimizer = optim.Adam(model.parameters(), lr=0.0001)
     else:
         if args.optimizer == 'SGD':
-            optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay = 0.0001)
+            optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay = 0.001)
     if args.loss == 'cross_ent':
         criterion = nn.CrossEntropyLoss()
     else:
